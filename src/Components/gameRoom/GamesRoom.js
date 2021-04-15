5f3af37d101b69a28febb29460bb5fc33d25caf8
Ignacio <jalfaros@estudiantec.cr>
2021-04-15 10:28:43 -0600

    Fixes

--- src/Components/gameRoom/GamesRoom.js
+++ src/Components/gameRoom/GamesRoom.js
@@ -17,18 +17,18 @@ export const GamesRoom = () => {
                             <div className="col-sm" >
                                 <div className="card mt-3">
                                     <div className="card-header" style={{ textAlign: 'center' }} >
-                                        <strong> Reversi </strong>
+                                        <strong> Othello </strong>
                                     </div>
 
-                                    <div className = "card-body" style = {{ textAlign : 'center' }} >
-                                        <img src = "https://upload.wikimedia.org/wikipedia/commons/2/20/Othello-Standard-Board.jpg" 
-                                            style = {{ width: '400px', height: '300px' }} alt="Othello"
+                                    <div className="card-body" style={{ textAlign: 'center' }} >
+                                        <img src="https://upload.wikimedia.org/wikipedia/commons/2/20/Othello-Standard-Board.jpg"
+                                            style={{ width: '400px', height: '300px' }} alt="Othello"
                                         />
                                     </div>
 
-                                    <div className = "card-footer" style = {{ textAlign: 'center' }}>
+                                    <div className="card-footer" style={{ textAlign: 'center' }}>
                                         <div className="m-3">
-                                        <Link className="txt1" to="/lobby"> Play Othello </Link>
+                                            <Link className="txt1" to="/lobby"> Play Othello </Link>
                                         </div>
                                     </div>
                                 </div>
@@ -40,19 +40,21 @@ export const GamesRoom = () => {
                                         <strong> More games ... </strong>
                                     </div>
 
-                                    <div className = "card-body" style = {{ textAlign : 'center' }} >
-                                        <img src = "https://i1.wp.com/www.seniainternational.org/wp-content/uploads/2019/02/Coming-Soon-2-1.png?fit=1000%2C816&ssl=1" 
-                                            style = {{ width: '400px', height: '300px' }} alt="Othello"
+                                    <div className="card-body" style={{ textAlign: 'center' }} >
+                                        <img src="https://i1.wp.com/www.seniainternational.org/wp-content/uploads/2019/02/Coming-Soon-2-1.png?fit=1000%2C816&ssl=1"
+                                            style={{ width: '400px', height: '300px' }} alt="Othello"
                                         />
                                     </div>
-                                    
-                                </div>
-                                <div className = "card-footer" style = {{ textAlign: 'center' }}>
 
+                                </div>
+                                <div className="card-footer" style={{ textAlign: 'center' }}>
+                                    <div className="m-3">
+                                        <button className = "btn btn-outline-dark" disabled>Play...</button>
+                                    </div>
                                 </div>
                             </div>
 
-                            
+
 
 
                         </div>
